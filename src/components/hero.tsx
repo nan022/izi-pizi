2c1a7fb8741f399ef5ba354650b633565350ae41
nan022 <ronanda20ti@mahasiswa.pcr.ac.id>
2025-12-05 21:39:33 +0700

    Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,7 @@
 // components/hero.tsx
 export default function Hero() {
   return (
-    <div className="relative z-10 flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-120">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-foreground mb-2">
           Izi Pizi
