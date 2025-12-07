c303c29726e204f21b3ddbb6560010e4004f5510
nan022 <ronanda20ti@mahasiswa.pcr.ac.id>
2025-12-08 00:45:36 +0700

    Setup Prisma

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,32 @@
+// app/page.tsx
+"use client";
+
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import { Particles } from "@/components/ui/shadcn-io/particles";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function Dashboard() {
+  const { resolvedTheme } = useTheme();
+  const [particleColor, setParticleColor] = useState("#ffffff");
+
+  useEffect(() => {
+    const currentTheme = resolvedTheme === "dark" ? "dark" : "light";
+    setParticleColor(currentTheme === "dark" ? "#ffffff" : "#000000");
+  }, [resolvedTheme]);
+
+  return (
+    <div className="relative min-h-screen bg-background">
+      <Header />
+      <Particles
+        className="absolute inset-0"
+        quantity={100}
+        ease={80}
+        staticity={50}
+        color={particleColor}
+        size={0.8}
+      />
+    </div>
+  );
+}
